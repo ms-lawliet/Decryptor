da9867a1e90ee98c120d2527be57ce0d54ec6da5
Ms Lawliet <alexieanoya@gmail.com>
2023-04-15 12:13:44 +0800

    made program description

---
+++ program_code.py
@@ -0,0 +1,3 @@
+# Write a Python Script that will accept a string as encrypted text and then
+# the program will decrypt it using the following character substitute:
+# 'a' = *, 'e' = & , 'i' = # , 'o' = + 'u' = !
