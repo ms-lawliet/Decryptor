2235e8d28bd55610413170f6005706ad203eae6b
Ms Lawliet <alexieanoya@gmail.com>
2023-04-15 12:19:40 +0800

    created pseudocode for program flow

--- program_code.py
+++ program_code.py
@@ -1,3 +1,12 @@
 # Write a Python Script that will accept a string as encrypted text and then
 # the program will decrypt it using the following character substitute:
 # 'a' = *, 'e' = & , 'i' = # , 'o' = + 'u' = !
+
+# take in string input from user
+# replace each character with respective letter substitute:
+# '*' for 'a'
+# '&' for 'e'
+# '#' for 'i'
+# '+' for 'o'
+# '!' for 'u'
+# print decrypted string
